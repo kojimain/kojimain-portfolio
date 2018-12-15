af50124ba1d6b3ed879af3e495e607c698d17e5f
kojimain <kojimainjp@gmail.com>
2018-12-15 21:23:23 +0900

    トップのブログ記事一覧をコンポーネント化

---
+++ components/index/Articles.vue
@@ -0,0 +1,30 @@
+<template>
+  <section class="section">
+    <h2 class="title has-text-centered">Blog</h2>
+    <div class="columns is-multiline">
+      <Article
+        v-for="article in articles"
+        :key="article.id"
+        :article="article"/>
+    </div>
+    <PoweredByGraphCms/>
+  </section>
+</template>
+
+<script>
+import articlesGql from '~/apollo/queries/index/articles.gql'
+import Article from './Article'
+import PoweredByGraphCms from '~/components/PoweredByGraphCms'
+
+export default {
+  apollo: {
+    articles: {
+      query: articlesGql
+    }
+  },
+  components: {
+    Article,
+    PoweredByGraphCms
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,30 +1,15 @@
 <template>
-  <section class="section">
-    <h2 class="title has-text-centered">Blog</h2>
-    <div class="columns is-multiline">
-      <Article
-        v-for="article in articles"
-        :key="article.id"
-        :article="article"/>
-    </div>
-    <PoweredByGraphCms/>
-  </section>
+  <div>
+    <Articles/>
+  </div>
 </template>
 
 <script>
-import articlesGql from '~/apollo/queries/index/articles.gql'
-import Article from '~/components/index/Article'
-import PoweredByGraphCms from '~/components/PoweredByGraphCms'
+import Articles from '~/components/index/Articles'
 
 export default {
-  apollo: {
-    articles: {
-      query: articlesGql
-    }
-  },
   components: {
-    Article,
-    PoweredByGraphCms
+    Articles
   }
 }
 </script>
