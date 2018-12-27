d917f49b1e39c49a74f70eaaeddc6de56d5f2cf9
kojimain <kojimainjp@gmail.com>
2018-12-27 14:33:20 +0900

    トップにRaspTemperaturesGraph追加

---
+++ components/index/RaspTemperaturesGraph.vue
@@ -0,0 +1,104 @@
+<template>
+  <section class="section">
+    <h2 class="title has-text-centered">Graph</h2>
+    <div class="has-text-centered">
+      <iframe
+        class="hatenablogcard"
+        style="width:100%;height:155px;margin:15px 0;max-width:680px;"
+        title="Raspberry Pi のCPU温度をfirebaseの Cloud Firestore に記録してVue.jsでグラフ表示する - kojimainjp’s blog"
+        src="https://hatenablog-parts.com/embed?url=https://kojimainjp.hatenablog.com/entry/2018/12/26/202942"
+        frameborder="0"
+        scrolling="no"/>
+    </div>
+    <div class="columns is-centered">
+      <div class="column is-6 box">
+        <div>
+          <vue-c3 :handler="handler"/>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+import Vue from 'vue'
+import firebase from 'firebase/app'
+import 'firebase/firestore'
+import VueC3 from 'vue-c3'
+import 'c3/c3.min.css'
+
+// firebase
+firebase.initializeApp({
+  projectId: 'portfolio-6edac'
+})
+const firestore = firebase.firestore()
+firestore.settings({ timestampsInSnapshots: true })
+
+export default {
+  components: {
+    VueC3
+  },
+  data() {
+    return {
+      temperatures: [],
+      handler: new Vue()
+    }
+  },
+  mounted() {
+    this.initGraph()
+  },
+  methods: {
+    async getTemperatures() {
+      await firestore
+        .collection('rasp_temperatures')
+        .orderBy('sentAt', 'asc')
+        .limit(288)
+        .get()
+        .then(querySnapshot => {
+          this.temperatures = querySnapshot.docs.map(doc => {
+            const data = doc.data()
+            return {
+              value: data.value,
+              sentAt: data.sentAt.toDate()
+            }
+          })
+        })
+    },
+    async initGraph() {
+      await this.getTemperatures()
+      const options = {
+        data: {
+          x: 'x',
+          columns: [
+            ['x'].concat(
+              this.temperatures.map(t => {
+                return t.sentAt
+              })
+            ),
+            ['温度(℃)'].concat(
+              this.temperatures.map(t => {
+                return t.value
+              })
+            )
+          ]
+        },
+        axis: {
+          x: {
+            type: 'timeseries',
+            tick: {
+              format: '%Y-%m-%d %H:%M',
+              culling: {
+                max: 2
+              }
+            }
+          }
+        },
+        size: {
+          height: 300
+        }
+      }
+      this.handler.$emit('init', options)
+    }
+  }
+}
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -2,17 +2,20 @@
   <div>
     <Articles/>
     <BbsThreads/>
+    <RaspTemperaturesGraph/>
   </div>
 </template>
 
 <script>
 import Articles from '~/components/index/Articles'
 import BbsThreads from '~/components/index/BbsThreads'
+import RaspTemperaturesGraph from '~/components/index/RaspTemperaturesGraph'
 
 export default {
   components: {
     Articles,
-    BbsThreads
+    BbsThreads,
+    RaspTemperaturesGraph
   }
 }
 </script>
